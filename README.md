dcae9bd9af2eb5ce357e9d1c9348aeb37b302050
Maru Esguerra <162283602+MaruEsguerra@users.noreply.github.com>
2025-02-21 11:00:43 +0800

    feat: Adds basic math functions Adds basic math functions to compute numbers.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # js-calculator
-My final project under TOP Foundations
+My final project under TOP Foundations. Here, I hope to create a simple but clean looking calculator that does basic mathematics on integers and potentially floating numbers too.
---
+++ javascript.js
@@ -0,0 +1,25 @@
+// Basic math functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Testing
+let a = 1;
+let b = 2;
+sum = add(a, b);
+difference = subtract(a, b);
+product = multiply(a, b);
+quotient = divide(a, b);
+console.log(sum, difference, product, quotient);
